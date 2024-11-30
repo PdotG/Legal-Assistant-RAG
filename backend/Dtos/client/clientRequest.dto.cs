351886cbd0df01c9e37edccec7e652333c8c000b
Kevin Sánchez <5795125+kevinxe@users.noreply.github.com>
2024-11-30 21:01:35 +0100

    client controller finished

--- backend/Dtos/client/clientRequest.dto.cs
+++ backend/Dtos/client/clientRequest.dto.cs
@@ -2,8 +2,8 @@ namespace backend.Dtos
 {
     public class ClientRequestDto
     {
-        public string Name { get; set; } = string.Empty;
-        public string ContactInformation { get; set; } = string.Empty;
+        public required string Name { get; set; } = string.Empty;
+        public required string ContactInformation { get; set; } = string.Empty;
         public string? Address { get; set; }
         public string? Notes { get; set; }
     }
