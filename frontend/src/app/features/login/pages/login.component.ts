e66bfd67824a08e275cb0d52561e5d76e8aafc6b
pablo <pablo@pablo.pablo>
2024-12-06 17:46:58 +0100

    first attempt to make an angular form

--- frontend/src/app/features/login/pages/login.component.ts
+++ frontend/src/app/features/login/pages/login.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { NgModule } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
+  imports: [BrowserModule, FormsModule]
 })
 export class LoginComponent {
+  showPassword: boolean = false;
 
+  onSubmit(form: NgForm) {
+    // Handle login logic here
+  }
 }
