093a3143ee175d53deec3eb000f0531162c4f91e
manu <manuabril2001@outlook.es>
2024-12-11 17:58:11 +0100

    fixed bug on upload service  and added error page

--- frontend/src/app/features/documents/data/upload.service.ts
+++ frontend/src/app/features/documents/data/upload.service.ts
@@ -1,17 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpEventType } from '@angular/common/http';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { FileUploadDto } from './file';
 import { environment } from '../../../../environments/environment';
-
 @Injectable({
   providedIn: 'root',
 })
 export class UploadService {
   private apiUrl = `${environment.apiUrl}/api/files`;
-  private uploadProgress = new BehaviorSubject<number>(0);
-  uploadProgress$ = this.uploadProgress.asObservable();
 
   constructor(private http: HttpClient) {}
 
@@ -21,27 +17,7 @@ export class UploadService {
     formData.append('name', name);
     formData.append('scrapedAt', new Date().toISOString());
 
-    return this.http
-      .post<FileUploadDto>(this.apiUrl, formData, {
-        reportProgress: true,
-        observe: 'events',
-      })
-      .pipe(
-        tap((event) => {
-          if (event.type === HttpEventType.UploadProgress) {
-            const progress = Math.round(
-              100 * (event.loaded / (event.total || 1))
-            );
-            this.uploadProgress.next(progress);
-          }
-        }),
-        map((event) => {
-          if (event.type === HttpEventType.Response) {
-            return event.body as FileUploadDto;
-          }
-          return null;
-        })
-      );
+    return this.http.post<FileUploadDto>(this.apiUrl, formData);
   }
 
   getAllFiles(): Observable<FileUploadDto[]> {
