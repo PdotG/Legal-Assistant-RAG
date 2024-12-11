20cd4037961499d7f01ca75eed8dfb2f03e557a2
manu <manuabril2001@outlook.es>
2024-12-11 19:01:28 +0100

    added frontpage

--- frontend/src/app/shared/ui/home/home.component.ts
+++ frontend/src/app/shared/ui/home/home.component.ts
@@ -5,8 +5,6 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-home',
   imports: [RouterOutlet],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
