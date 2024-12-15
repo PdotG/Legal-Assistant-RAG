4d270f114dd74e81a4e552783be36e9875c4f213
manu <manuabril2001@outlook.es>
2024-12-15 19:51:55 +0100

    change the mainpage component

--- frontend/src/app/features/mainpage/mainpage.component.ts
+++ frontend/src/app/features/mainpage/mainpage.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, Router } from '@angular/router';
+import { LoginService } from '../login/data/login.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-mainpage',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './mainpage.component.html',
-  styleUrl: './mainpage.component.css'
+  styleUrl: './mainpage.component.css',
 })
-export class MainpageComponent {
-
+export class MainpageComponent implements OnInit {
+  isLoggedIn$!: Observable<boolean>;
+  constructor(private loginService: LoginService, private router: Router) {}
+  ngOnInit(): void {
+    this.isLoggedIn$ = this.loginService.isLoggedIn$;
+  }
 }
