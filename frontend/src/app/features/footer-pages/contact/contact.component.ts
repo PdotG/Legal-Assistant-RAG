9e4505456dfe24dbde0667c8dff634b8b44ba61c
Kevin Sánchez <5795125+kevinxe@users.noreply.github.com>
2024-12-19 17:34:46 +0100

    Changes in chat

--- frontend/src/app/features/footer-pages/contact/contact.component.ts
+++ frontend/src/app/features/footer-pages/contact/contact.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
